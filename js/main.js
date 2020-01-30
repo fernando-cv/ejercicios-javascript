bbe333eeb7453e917e3922ca56ae2a4bc496b92c
fernando <fernando.c@viacodigo.com>
2020-01-30 12:51:23 -0500

    aleatoria

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 1
+
+var numero = Math.round(Math.random() * 99);
+if (numero > 1 && numero < 99) {
+  document.write(numero);
+}
+console.log(numero)
+
+// 2
+
+var username = prompt("¿Cuál es su nombre?");
+username = username.toUpperCase();
+console.log(username);
+
+// 3
+
+
+
+
+
